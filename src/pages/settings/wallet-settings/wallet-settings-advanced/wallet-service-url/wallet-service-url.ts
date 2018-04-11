345796c08a0d0643524c565f9bbb26eeaef09aca
Ryan Zhu <zhutao@genyuanlian.com>
2018-04-11 19:09:21 +0800

    Update wallet service url

--- src/pages/settings/wallet-settings/wallet-settings-advanced/wallet-service-url/wallet-service-url.ts
+++ src/pages/settings/wallet-settings/wallet-settings-advanced/wallet-service-url/wallet-service-url.ts
@@ -67,7 +67,7 @@ export class WalletServiceUrlPage {
       case 'prod':
       case 'production':
         // bws = 'https://bws.bitpay.com/bws/api'
-        bws= 'http://218.90.198.55:3232/bws/api'
+        bws= 'http://119.28.19.103:3232/bws/api'
         break;
       case 'sta':
       case 'staging':
