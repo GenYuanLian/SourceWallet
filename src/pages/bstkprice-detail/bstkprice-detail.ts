054f9afcbb0489800e4c60e8a11ca0afe60aefae
Ryan Zhu <zhutao@genyuanlian.com>
2018-07-05 19:46:58 +0800

    Adjust style; add open exchange site;

--- src/pages/bstkprice-detail/bstkprice-detail.ts
+++ src/pages/bstkprice-detail/bstkprice-detail.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from "@angular/core";
 import { BstkPriceProvider } from '../../providers/bstk-price/bstk-price';
+import { ExternalLinkProvider } from '../../providers/external-link/external-link';
 
 @Component({
   selector: 'bstkprice-detail',
@@ -8,12 +9,33 @@ import { BstkPriceProvider } from '../../providers/bstk-price/bstk-price';
 
 export class BstkPriceDetailPage {
   constructor (
-    private bstkPriceProvider: BstkPriceProvider
+    private bstkPriceProvider: BstkPriceProvider,
+    private externalLinkProvider: ExternalLinkProvider,
   ) {
 
   }
 
   get priceList() {
     return this.bstkPriceProvider.priceList;
+  }
+
+  public openExchangeWeb(item: any): void {
+    console.log('item:', item);
+    let bitzExchange = "https://www.bit-z.pro/exchange/bstk_";
+    let c2cExchange = "https://coin2coin.jp/exchange/";
+
+    let url = '';
+    if (item.marketName === 'C2C') {
+      url += c2cExchange + item.pairName + '_BSTK';
+    } else {
+      url += bitzExchange + item.pairName.toLowerCase();
+    }
+
+    let optIn = true;
+    let title = null;
+    let message = '是否访问交易所页面';
+    let okText = '确认';
+    let cancelText = '取消';
+    this.externalLinkProvider.open(url, optIn, title, message, okText, cancelText);
   }
 }
